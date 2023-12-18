58bf96048fa5fc3d751e9308066aa8875ebf92f6
amadeo <amadeodeg@gmail.com>
2023-12-18 15:05:59 +0100

    day17 forgot to commit

---
+++ day17/17a.py
@@ -0,0 +1,35 @@
+import heapq
+file1 = open('./day17/input.txt', 'r') 
+#file1 = open('./day17/example.txt', 'r') 
+Lines = [l.strip() for l in file1.readlines()]
+
+M={}
+for y, l in enumerate(Lines):
+    for x, c in enumerate(l):
+        M[(x,y)]=int(c)
+maxY=y
+maxX=x
+#item = (cost,x,y,bx,by) bx,by banned direction
+result=0
+visited=set()
+toVisit=[(0,0,0,0,0)]
+while len(toVisit):
+    cost,x,y,bx,by = heapq.heappop(toVisit)
+    if (x,y)==(maxX,maxY):
+        result=cost
+        break
+    if (x,y,bx,by) in visited:
+        continue
+    visited.add((x,y,bx,by))
+    for dx,dy in [(0,-1),(1,0),(0,1),(-1,0)]:
+        if (dx,dy)==(bx,by) or (dx,dy)==(-bx,-by):
+            continue
+        xaux,yaux,costaux=x,y,cost
+        for d in range(1,4):
+            xaux=xaux+dx
+            yaux=yaux+dy
+            if (xaux,yaux) in M:
+                costaux+=M[(xaux,yaux)]
+                heapq.heappush(toVisit,(costaux,xaux,yaux,dx,dy))
+print(result)  
+print("hello")
